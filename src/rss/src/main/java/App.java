6604955914be2255f098ed7c5c7be1280d27314b
Truc Thai <truc.thai-nguyen@hp.com>
2016-08-07 02:27:07 +0700

    rss + parse html

--- src/rss/src/main/java/App.java
+++ src/rss/src/main/java/App.java
@@ -3,10 +3,13 @@ import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
@@ -18,7 +21,7 @@ public class App {
     public static void main(String[] args) throws IOException, FeedException {
         System.out.println("hello");
 
-        URL url = new URL("http://www.bongda.com.vn/feed.rss");
+        URL url = new URL("http://vnexpress.net/rss/tin-moi-nhat.rss");
         HttpURLConnection httpcon = (HttpURLConnection)url.openConnection();
         // Reading the feed
         SyndFeedInput input = new SyndFeedInput();
@@ -33,7 +36,17 @@ public class App {
             System.out.println("Author: " + entry.getAuthor());
             System.out.println("Publish Date: " + entry.getPublishedDate());
             System.out.println("Description: " + entry.getDescription().getValue());
+            System.out.println(getUrlContent(entry.getLink()));
             System.out.println();
+
         }
+    }
+
+    private static String getUrlContent(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Element content = doc.select("div.fck_detail").first();
+        if (content != null)
+            return content.text();
+        return "Empty text!!";
     }
 }
