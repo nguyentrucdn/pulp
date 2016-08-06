229655b6ebbcd77cf4f97b01e58c146637159351
Truc Thai <truc.thai-nguyen@hp.com>
2016-08-07 01:22:50 +0700

    rss

--- src/rss/src/main/java/App.java
+++ src/rss/src/main/java/App.java
@@ -1,9 +1,39 @@
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by thainguy on 8/7/2016.
  */
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, FeedException {
         System.out.println("hello");
 
+        URL url = new URL("http://www.bongda.com.vn/feed.rss");
+        HttpURLConnection httpcon = (HttpURLConnection)url.openConnection();
+        // Reading the feed
+        SyndFeedInput input = new SyndFeedInput();
+        SyndFeed feed = input.build(new XmlReader(httpcon));
+        List entries = feed.getEntries();
+        Iterator itEntries = entries.iterator();
+
+        while (itEntries.hasNext()) {
+            SyndEntry entry = (SyndEntry) itEntries.next();
+            System.out.println("Title: " + entry.getTitle());
+            System.out.println("Link: " + entry.getLink());
+            System.out.println("Author: " + entry.getAuthor());
+            System.out.println("Publish Date: " + entry.getPublishedDate());
+            System.out.println("Description: " + entry.getDescription().getValue());
+            System.out.println();
+        }
     }
 }
